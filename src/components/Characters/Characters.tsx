47ad81670a7ed5f4a9dd85848a2a4a5c8acaddbe
Yaroslav Makarov <makarov160393@gmail.com>
2021-03-09 19:02:29 +0200

    added markup to characters page

--- src/components/Characters/Characters.tsx
+++ src/components/Characters/Characters.tsx
@@ -1,7 +1,10 @@
 import { useEffect, Dispatch } from "react";
 import { useDispatch, useSelector } from "react-redux";
+
+import './Characters.scss';
 import { CharactersThunk, loadingPersons } from "../../store/charactersReucer";
 import { churactersSelector } from "../../store/rootStore";
+import Character from "../Character/Character";
 
 const Characters = () => {
     const personThunkDispatch = useDispatch<Dispatch<CharactersThunk>>();
@@ -12,12 +15,14 @@ const Characters = () => {
         personThunkDispatch(loadingPersons());
     }, []);
 
-    console.log(characters);
-    
-
     return (
-        <div>
-            Characters
+        <div className="characters__wrapper">
+            {characters.map(character => (
+                <Character
+                    key={character.id}
+                    character={character}
+                />
+            ))}
         </div>
     );
 };
