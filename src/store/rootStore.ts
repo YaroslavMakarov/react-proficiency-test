dc729744caaacfed598ac98227558a3fdc37fd4f
Yaroslav Makarov <makarov160393@gmail.com>
2021-03-09 11:36:12 +0200

    added characterReducer to rootStore

--- src/store/rootStore.ts
+++ src/store/rootStore.ts
@@ -1,10 +1,16 @@
-import { combineReducers, createStore, Store } from 'redux';
+import { combineReducers, createStore, Store, applyMiddleware } from 'redux';
+import thunk from "redux-thunk";
+
+import charactersReducer, { InitialCharactersState } from './charactersReucer';
 
 const allReducers = combineReducers({
+    characters: charactersReducer,
 });
 
-export type State = {};
+export type State = {
+    characters: InitialCharactersState,
+};
 
-const store: Store<State> = createStore(allReducers);
+const store: Store<State> = createStore(allReducers, applyMiddleware(thunk));
 
 export default store;
