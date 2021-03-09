b3c5fc52b164001c8f04df2f95c3c82425498f26
Yaroslav Makarov <makarov160393@gmail.com>
2021-03-09 21:38:52 +0200

    added IntersectionObserver

--- src/components/Characters/Characters.tsx
+++ src/components/Characters/Characters.tsx
@@ -1,4 +1,4 @@
-import { useEffect, Dispatch } from "react";
+import { useEffect, Dispatch, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import './Characters.scss';
@@ -9,21 +9,39 @@ import Character from "../Character/Character";
 const Characters = () => {
     const personThunkDispatch = useDispatch<Dispatch<CharactersThunk>>();
 
+    const pageEnd = useRef<HTMLDivElement>(null);
+
     const characters = useSelector(churactersSelector);
 
     useEffect(() => {
         personThunkDispatch(loadingPersons());
     }, []);
 
+    useEffect(() => {
+        const observer = new IntersectionObserver((entries) => {
+            console.log(entries[0].isIntersecting);
+            
+        },
+        { rootMargin: '200px' })
+        if (pageEnd.current !== null) {
+            observer.observe(pageEnd.current)
+        }
+    }, [])
+
     return (
-        <div className="characters__wrapper">
-            {characters.map(character => (
-                <Character
-                    key={character.id}
-                    character={character}
-                />
-            ))}
-        </div>
+        <>
+            <div className="characters__wrapper">
+                {characters.map(character => (
+                    <Character
+                        key={character.id}
+                        character={character}
+                    />
+                ))}
+            </div>
+            <div className="characters__lazy-loading"ref={pageEnd}>
+                Loading
+            </div>
+        </>
     );
 };
 
